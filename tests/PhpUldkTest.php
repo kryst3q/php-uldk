140a6cbfbd96f1878df31bb40e09f50b67c45408
Krystian Kulesz <krystian.kulesz@gmail.com>
2020-03-12 00:29:17 +0100

    Move symfony/var-dumper to dev dependencies & downgrade its version to stable one

--- tests/PhpUldkTest.php
+++ tests/PhpUldkTest.php
@@ -9,23 +9,4 @@ use Kryst3q\PhpUldk\PhpUldk;
 
 class PhpUldkTest extends Unit
 {
-    private string $parcelId = '141201_1.0001.6509';
-    private PhpUldk $phpUldk;
-
-    protected function _before()
-    {
-
-        $this->phpUldk = new PhpUldk();
-    }
-
-    protected function _after()
-    {
-    }
-
-    // tests
-    public function testSomeFeature()
-    {
-        $result = $this->phpUldk->getParcelById($this->parcelId);
-        self::assertInstanceOf(UldkObject::class, $result);
-    }
 }
